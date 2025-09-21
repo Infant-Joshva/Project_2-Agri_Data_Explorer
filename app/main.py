e3c3dda7e00912f1ded8599b9a354d3f0f47276b
Infant Joshva A <infantjoshva2024@gmail.com>
2025-09-21 17:44:27 +0530

    Mail ID changed

--- app/main.py
+++ app/main.py
@@ -1251,6 +1251,6 @@ st.markdown("""
         <p style="font-size: 20px;">🌿 Agri Data Explorer | Built by <strong>Infant Joshva</strong></p>
         <a href="https://github.com/Infant-Joshva" target="_blank" style="text-decoration: none; margin: 0 10px;">🐙 GitHub</a>
         <a href="https://www.linkedin.com/in/infant-joshva" target="_blank" style="text-decoration: none; margin: 0 10px;">🔗 LinkedIn</a>
-        <a href="mailto:infantjoshva46@gmail.com" style="text-decoration: none; margin: 0 10px;">📩 Contact</a>
+        <a href="mailto:infantjoshva2024@gmail.com" style="text-decoration: none; margin: 0 10px;">📩 Contact</a>
     </div>
 """, unsafe_allow_html=True)
